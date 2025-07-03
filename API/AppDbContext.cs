47a1579918be2bd91f6959528674a55e1f649213
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-07-04 06:56:38 +1000

    End of section 2

---
+++ API/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+  public DbSet<AppUser> Users { get; set; }
+  
+}
