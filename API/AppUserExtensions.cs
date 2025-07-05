0ce94c382522854798269ae02f47707623620260
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-07-06 06:15:00 +1000

    End of section 4

---
+++ API/AppUserExtensions.cs
@@ -0,0 +1,23 @@
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+
+namespace API;
+
+public static class AppUserExtensions
+{
+  public static UserDto ToDto(
+    this AppUser user, ITokenService tokenService)  
+  {
+    var userDto = new UserDto
+    {
+      Id = user.Id,
+      Email = user.Email,
+      DisplayName = user.DisplayName,
+      ImageUrl = null,
+      Token = tokenService.CreateToken(user)
+    };
+
+    return userDto;
+  }
+}
---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+  [HttpPost("register")]
+  public async Task<ActionResult<UserDto>> Register(RegisterDto dto)
+  {
+    if (await IsExistingEmail(dto.Email))
+    {
+      return BadRequest("Email already exists");
+    }
+
+    using var hmac = new HMACSHA512();
+    var user = new AppUser
+    {
+      DisplayName = dto.DisplayName,
+      Email = dto.Email,
+      PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password)),
+      PasswordSalt = hmac.Key
+    };
+
+    context.Users.Add(user);
+    await context.SaveChangesAsync();
+
+    var userDto =  user.ToDto(tokenService);
+
+    return userDto;
+  }
+
+  [HttpPost("login")]
+  public async Task<ActionResult<UserDto>> Login(LoginDto dto)
+  {
+    var user = await context.Users.SingleOrDefaultAsync(x =>
+      x.Email.ToLower() == dto.Email.ToLower());
+    if (user == null) return Unauthorized("Invalid email");
+
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+    var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dto.Password));
+
+    for (var i = 0; i < computedHash.Length; i++)
+    {
+      if (computedHash[i] != user.PasswordHash[i])
+      {
+        return Unauthorized("Invalid password");
+      }
+    }
+
+    var userDto =  user.ToDto(tokenService);
+
+    return userDto;
+  }
+
+  private async Task<bool> IsExistingEmail(string email)
+  {
+    return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+  }
+}
