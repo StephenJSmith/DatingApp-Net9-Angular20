50b4b8e350371580521234f9d02423b085114a78
Stephen Smith <stephensmith_aus@yahoo.com.au>
2025-07-06 16:41:07 +1000

    End of section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,27 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   
   protected readonly title = signal('Dating App');
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   async ngOnInit(): Promise<void> {
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+
+      const user = JSON.parse(userString);
+      this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
     try {
       const url = 'https://localhost:5001/api/members';
-      return firstValueFrom(this.http.get(url));
+      return firstValueFrom(this.http.get<User[]>(url));
     } catch (error) {
       console.log(error);
       throw error;
